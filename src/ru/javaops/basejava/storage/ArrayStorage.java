b811fbe51d1f1532352b7c8229d5a22feac4d452
Marina <marina-dm2000@mail.ru>
2023-06-12 20:57:17 +0300

    Release update and change other methods

---
+++ src/ru/javaops/basejava/storage/ArrayStorage.java
@@ -0,0 +1,92 @@
+package ru.javaops.basejava.storage;
+
+import ru.javaops.basejava.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private final Resume[] storage = new Resume[10000];
+    private int size;
+
+    public void clear() {
+        /* Удаление без Arrays с условием, что удаляются только те поля, где было резюме
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }*/
+
+        Arrays.fill(storage, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume with uuid " + r.getUuid() + " already exists.");
+            return;
+        }
+
+        if (size >= storage.length) {
+            System.out.println("Storage is full.");
+            return;
+        }
+
+        storage[size] = r;
+        size++;
+    }
+
+    public Resume get(String uuid) {
+        if (getIndex(uuid) == -1) {
+            System.out.println("Resume with uuid " + uuid + " is not exists.");
+            return null;
+        }
+
+        return storage[getIndex(uuid)];
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+
+        if (index == -1) {
+            System.out.println("Resume with uuid " + uuid + " is not exists.");
+            return;
+        }
+
+        storage[index] = storage[size - 1];
+        storage[size - 1] = null;
+        size--;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+
+        if (index == -1) {
+            System.out.println("Resume with uuid " + resume.getUuid() + " is not exists.");
+            return;
+        }
+
+        storage[index] = resume;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
