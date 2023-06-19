821a31f0a7f878106c5b6249b5d0bb0b766105e4
marina-dm2000 <marina_dm28@icloud.com>
2023-06-19 17:04:17 +0300

    Create reflection

--- src/ru/javaops/basejava/MainReflection.java
+++ src/ru/javaops/basejava/MainReflection.java
@@ -3,9 +3,11 @@ package ru.javaops.basejava;
 import ru.javaops.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -13,5 +15,10 @@ public class MainReflection {
         System.out.println(field.get(resume));
         field.set(resume, "new_uuid");
         System.out.println(resume);
+        field.setAccessible(false);
+
+        Class<Resume> resumeObject = Resume.class;
+        Method method = resumeObject.getDeclaredMethod("toString");
+        System.out.println(method.invoke(resume));
     }
 }
