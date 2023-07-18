a6faedce462d343119a4dd04a67d84c45b6c9c07
marina-dm2000 <marina_dm28@icloud.com>
2023-07-18 13:27:49 +0300

    lesson 8

---
+++ src/ru/javaops/basejava/MainFile.java
@@ -0,0 +1,30 @@
+package ru.javaops.basejava;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainFile {
+    public static void main(String[] args) {
+        String filePath = ".\\.gitignore";
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+
+        File dir = new File(".\\src\\ru\\javaops\\basejava");
+        System.out.println(dir.isDirectory());
+        for (String name : Objects.requireNonNull(dir.list())) {
+            System.out.println(name);
+        }
+
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
