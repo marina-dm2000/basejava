a78abccaba546636ff88d615ddfad04e4835c67e
marina-dm2000 <marina_dm28@icloud.com>
2023-08-08 16:37:05 +0300

    lesson 12

--- src/ru/javaops/basejava/MainConcurrency.java
+++ src/ru/javaops/basejava/MainConcurrency.java
@@ -2,13 +2,28 @@ package ru.javaops.basejava;
 
 import ru.javaops.basejava.util.DeadLock;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private static int counter;
-    private static final Object LOCK = new Object();
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+    //private static final Object LOCK = new Object();
+    //private static final Lock lock = new ReentrantLock();
+    private static final ReentrantReadWriteLock REENTRANT_READ_WRITE_LOCK = new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = REENTRANT_READ_WRITE_LOCK.writeLock();
+    private static final Lock READ_LOCK = REENTRANT_READ_WRITE_LOCK.readLock();
+    private static final ThreadLocal<SimpleDateFormat> SDT = ThreadLocal.withInitial(SimpleDateFormat::new);
+
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -16,7 +31,7 @@ public class MainConcurrency {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
+                //throw new IllegalStateException();
             }
         };
         thread0.start();
@@ -38,27 +53,40 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        //CompletionService completionService = new ExecutorCompletionService(executorService);
+
+        //List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+            Future<Integer> future = executorService.submit(() ->
+                    //Thread thread = new Thread(() ->
+            {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
+                    System.out.println(SDT.get().format(new Date()));
                 }
+                latch.countDown();
+                return 5;
             });
 
-            thread.start();
-            threads.add(thread);
+            //thread.start();
+            //threads.add(thread);
         }
 
-        threads.forEach(t -> {
+
+        /*threads.forEach(t -> {
             try {
                 t.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        });
+        });*/
 
-        System.out.println(counter);
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+        //System.out.println(counter);
+        System.out.println(mainConcurrency.atomicCounter.get());
 
         String obj1 = "object1";
         String obj2 = "object2";
@@ -68,7 +96,14 @@ public class MainConcurrency {
         deadLock2.threadStart();
     }
 
-    private synchronized void inc() {
-        counter++;
+    private void inc() {
+        atomicCounter.incrementAndGet();
+
+        /*lock.lock();
+        try {
+            counter++;
+        } finally {
+            lock.unlock();
+        }*/
     }
 }
